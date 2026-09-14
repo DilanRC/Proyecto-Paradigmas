f5f63909efe19ce351e4a5ecb14379a7f381207a
Dilan Josue Rodríguez Cortes <129096951+DilanRC@users.noreply.github.com>
2026-09-14 10:00:27 -0600

    Front 2.0: permite retorno a fletes tras login

--- Public/js/login.js
+++ Public/js/login.js
@@ -7,7 +7,7 @@ const PRIVATE_ROUTES = new Set([
     'vehiculos.php',
     'pagometodos.php',
 ]);
-const PUBLIC_DESTINATIONS = new Set(['explorar.php', 'mi-actividad.php']);
+const PUBLIC_DESTINATIONS = new Set(['explorar.php', 'mi-actividad.php', 'fletes.php']);
 
 export function resolveNext(search = '', hasProfile = false) {
     const requested = new URLSearchParams(search).get('next');
